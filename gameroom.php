9f5f35203089c81f71ae846797a3f3fb1e371d48
Joe <ja582@njit.edu>
2019-11-28 19:28:26 -0500

    adding in win/lost scenarios

--- gameroom.php
+++ gameroom.php
@@ -1,14 +1,24 @@
 <?php
 session_start();
 include_once("blade/functions.php");
+require("config.php");
+$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+$db = new PDO($conn_string, $username, $password);
+
+
+function takeMoney(){
+    $user_idN = $_SESSION['user']['id'];
+    $query = "UPDATE `Users` SET `bal`= bal - 1 WHERE `id` = (:user_id)";
+    $stmt = $db->prepare($query);
+    $r = $stmt->execute(array(":user_id"=>$user_idN));
+    $results = $stmt->fetch(PDO::FETCH_ASSOC);
+    return true;
+}
+
 
 if(isset($_POST['submit'])){
     $playerChoice = $_POST['submit'];
     $computerRandInt = rand(1,3);
-
-    $winCount = 0;
-    $loseCount = 0;
-
     switch($computerRandInt){
         case 1:
             $computerChoice = "Rock";
@@ -28,33 +38,32 @@ if(isset($_POST['submit'])){
     if($computerChoice == "Rock" && $playerChoice == "Paper"){
         echo "<br />";
         echo "win!!";
+    }else if($computerChoice == "Paper" && $playerChoice == "Rock"){
+        echo "<br />";
+        echo "you lose!!";
     }
     if($computerChoice == "Paper" && $playerChoice == "Scissor"){
         echo "<br />";
         echo "win!!";
+    }else if($computerChoice == "Scissor" && $playerChoice == "Paper"){
+        echo "<br />";
+        echo "you lose!!";
     }
     if($computerChoice == "Scissor" && $playerChoice == "Rock"){
         echo "<br />";
         echo "win!!";
-    }
-    //mess of if statements.. sorry
-    if($computerChoice == "Scissor" && $playerChoice == "Paper"){
+    }else if($computerChoice == "Rock" && $playerChoice == "Scissor"){
         echo "<br />";
-        echo "lose!!";
-    }
-    if($computerChoice == "Paper" && $playerChoice == "Rock"){
+        echo "you lose!!";
+        takeMoney();
         echo "<br />";
-        echo "lose!!";
-    }
-    if($computerChoice == "Rock" && $playerChoice == "Scissor"){
-        echo "<br />";
-        echo "lose!!";
     }
     if($computerChoice == $playerChoice){
         echo "<br />";
-        echo "draw!";
+        echo "draww!";
     }
-
+}else{
+    "NO MONEY";
 }
 
 
